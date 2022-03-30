66fb47bcdb749dbde44a718c4e3028c8ba800d31
Alyona <alyonacherepanova6954@gmail.com>
2022-03-30 12:21:30 +0300

    add start and finish output

--- code/map_drawing.py
+++ code/map_drawing.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 def map_drawing (n, m, map, start_point, finish_point, 
                 robot_path):
 
@@ -8,37 +7,52 @@ def map_drawing (n, m, map, start_point, finish_point,
         print ("Error: invalid map size")
         return
 
-    max_size_dis_side = 800
-    size_cell = max_size_dis_side // max (n, m)
+    max_size_screen_side = 800
+    size_cell = max_size_screen_side // max (n, m)
 
-    print (size_cell, n, m)
-
+    # init screen 
     pygame.init()
-    dis = pygame.display.set_mode((size_cell * m, size_cell * n))
+    screen = pygame.display.set_mode((size_cell * m, size_cell * n))
 
     pygame.display.update()
     pygame.display.set_caption('Mars Rover movement')
+    
+    # text style
+    words_font = pygame.font.SysFont("comicsansms", 35)
 
-    blue = (0, 0, 255)
+    # color
     white = (255, 255, 255)
+    red = (213, 50, 80)
+    blue = (50, 153, 213)
+    obstacle_color = blue
+    background_color = white
+    text_color = red
 
-    game_over = False
-    while not game_over:
+    
+    # drawing map
+    show_screen = False
+    while not show_screen:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game_over = True
+                show_screen = True
     
         for i in range(n):
             for j in range (m):
 
                 if map[i][j] == 1:
-                    color = blue
+                    color = obstacle_color
                 else:
-                    color = white
+                    color = background_color
 
-                pygame.draw.rect(dis, color, [j * size_cell, i * size_cell,
+                pygame.draw.rect(screen, color, [j * size_cell, i * size_cell,
                                  size_cell, size_cell])
         
+        
+        screen.blit(words_font.render("start", True, text_color),
+                 dest = (size_cell * start_point[1], size_cell * start_point[0]))
+        screen.blit(words_font.render("finish", True, text_color),
+                 dest = (size_cell * finish_point[1], size_cell * finish_point[0]))
+
         pygame.display.update()
 
     pygame.quit()

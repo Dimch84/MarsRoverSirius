1ac7e5070408acdce7669ed1ef94dc7a977aea6f
Alyona <alyonacherepanova6954@gmail.com>
2022-04-12 21:30:51 +0300

    make static map

--- code/map_drawing.py
+++ code/map_drawing.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import CENTER, LAST, Tk, Canvas, Frame, BOTH
 
 from map import Map
 
@@ -9,19 +9,11 @@ class Draw(Frame):
         self.initUI(map)
  
     def initUI(self, map : Map):
-        self.master.title("Рисуем формы")
-        self.pack(fill=BOTH, expand=1)
+
+        self.master.title("Map")
+        self.pack(fill = BOTH, expand = 1)
  
         canvas = Canvas(self)
-
-        # #draw lines
-        # for i in range (map.n):
-        #     canvas.create_line (0, map.size_cell * i,
-        #                        map.width, map.size_cell * i)
-
-        # for i in range (map.m):
-        #     canvas.create_line (map.size_cell * i, 0,
-        #                        map.size_cell * i, map.height)
 
         # draw cell
         for i in range (map.m):
@@ -36,7 +28,50 @@ class Draw(Frame):
                         map.size_cell * (i + 1), map.size_cell * (j + 1),
                         outline = 'black', fill = color_cell, width = 1)
 
-        canvas.pack(fill=BOTH, expand=1)
+        
+        # drawing movement Mars rover
+        mars_rover_coordinate_y = map.start_point[0]
+        mars_rover_coordinate_x = map.start_point[1]
+        for step in map.mars_rover_path:
+            
+            new_mars_rover_coordinate_y = mars_rover_coordinate_y
+            new_mars_rover_coordinate_x = mars_rover_coordinate_x
+            
+            # next coordinate
+            if step == 'U':
+                new_mars_rover_coordinate_y -= 1
+            if step == 'D':
+                new_mars_rover_coordinate_y += 1
+            if step == 'R':
+                new_mars_rover_coordinate_x += 1
+            if step == 'L':
+                new_mars_rover_coordinate_x -= 1
+
+            canvas.create_line(
+                            mars_rover_coordinate_x * map.size_cell + map.size_cell // 2,
+                            mars_rover_coordinate_y * map.size_cell + map.size_cell // 2,
+                            new_mars_rover_coordinate_x * map.size_cell + map.size_cell // 2,
+                            new_mars_rover_coordinate_y * map.size_cell + map.size_cell // 2,
+                            arrow = LAST,
+                            dash = (5, 2))
+
+
+            mars_rover_coordinate_x = new_mars_rover_coordinate_x
+            mars_rover_coordinate_y = new_mars_rover_coordinate_y
+
+
+        #out start and finish
+        canvas.create_text(map.size_cell * map.start_point[1] + map.size_cell // 2, 
+                            map.size_cell * map.start_point[0] + map.size_cell // 2,
+                            text = 'START',
+                            justify = CENTER)
+        canvas.create_text(map.size_cell * map.finish_point[1] + map.size_cell // 2, 
+                            map.size_cell * map.finish_point[0] + map.size_cell // 2,
+                            text = 'FINISH',
+                            justify = CENTER)
+
+
+        canvas.pack(fill = BOTH, expand = 1)
  
  
 

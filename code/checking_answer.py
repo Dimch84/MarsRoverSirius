39f7f1768f0375207ed608b18f6f10ca8eb3288d
Alyona <alyonacherepanova6954@gmail.com>
2022-04-14 18:29:33 +0300

    added travel cost calculation for simple map

--- code/checking_answer.py
+++ code/checking_answer.py
@@ -1,6 +1,6 @@
 from map import Map
 
-def checking_the_answer_simple_map(map : Map) -> bool:
+def checking_the_answer_simple_map(map : Map) -> list:
     
     mars_rover_y = map.start_point[0]
     mars_rover_x = map.start_point[1]
@@ -19,14 +19,14 @@ def checking_the_answer_simple_map(map : Map) -> bool:
         # out-of-bounds check
         if (mars_rover_x < 0 or mars_rover_x >= map.m
             or mars_rover_y < 0 or mars_rover_y >= map.n):
-            return False
+            return (-1)
 
         # walking test
         if (map.map[mars_rover_y][mars_rover_x] == 1):
-            return False
+            return (-1)
 
     # checking that they came to the right place
     if (map.finish_point != (mars_rover_x, mars_rover_y)):
-        return False
+        return (-1)
     
-    return True
+    return (1, len(map.mars_rover_path))

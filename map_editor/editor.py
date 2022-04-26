fd096ee2942f5ad4ffd358bff4b837ba88256a78
sergeyrid <sergeyrid@yandex.ru>
2022-04-26 18:55:15 +0300

    Refactor to reduce size of field.py

--- map_editor/editor.py
+++ map_editor/editor.py
@@ -1,4 +1,4 @@
-from tkinter import Misc, Toplevel
+from tkinter import Event, Misc, Toplevel
 
 from map_editor.field import Field
 from map_editor.ui import UI
@@ -12,6 +12,8 @@ class Editor:
     """
     def __init__(self, master: Misc) -> None:
         self.__slave = Toplevel(master)
+        self.__field = None
+        self.__ui = None
         self.__configure()
 
     def __configure(self) -> None:
@@ -24,5 +26,54 @@ class Editor:
         self.__slave.geometry("{0}x{1}+0+0".format(
             self.__slave.winfo_screenwidth(),
             self.__slave.winfo_screenheight()))
-        field = Field(self.__slave)
-        UI(self.__slave, field)
+        self.__field = Field(self.__slave)
+        self.__ui = UI(self.__slave, self.__field)
+        self.__add_binds()
+
+    def __add_binds(self) -> None:
+        """
+        This method adds binds to this class to process user input.
+
+        :return:
+        """
+        self.__slave.focus_set()
+        self.__slave.bind('<KeyPress>', self.__process_key_press)
+        self.__slave.bind('<KeyRelease>', self.__process_key_release)
+
+    def __process_key_press(self, event: Event) -> None:
+        """
+        This method processes a key press event, it should be used in
+        widget.bind.
+
+        :param event: tkinter event.
+        :return:
+        """
+        key = event.keysym
+        if key == 'plus':
+            self.__field.zoom_in()
+        elif key == 'minus':
+            self.__field.zoom_out()
+        elif key == 'Right':
+            self.__field.move_right()
+        elif key == 'Left':
+            self.__field.move_left()
+        elif key == 'Down':
+            self.__field.move_down()
+        elif key == 'Up':
+            self.__field.move_up()
+        elif str.isdigit(key):
+            self.__field.change_square_type(int(key))
+
+    def __process_key_release(self, event: Event) -> None:
+        """
+        This method processes a key release event, it should be used in
+        widget.bind.
+
+        :param event: tkinter event.
+        :return:
+        """
+        key = event.keysym
+        if key == 'Alt_L' or key == 'Alt_R':
+            self.__alt_pressed = False
+        elif key == 'Shift_L' or key == 'Shift_R':
+            self.__shift_pressed = False

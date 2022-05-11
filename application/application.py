11e0c6c7f118378ad6b56d9dd8a70bea7c1f6b44
sergeyrid <sergeyrid@yandex.ru>
2022-05-11 11:25:24 +0300

    Add basic interface

--- application/application.py
+++ application/application.py
@@ -2,10 +2,12 @@ import json
 import tkinter
 from tkinter import *
 from inspect import getmro
-from json import load, loads
+from json import dump, dumps, load, loads
+from pathlib import PurePath
 from requests import get, post
 
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from tkinter.simpledialog import askstring
 
 from application.map import Map
 from application.map_drawing import draw
@@ -14,49 +16,74 @@ from map_editor.editor import Editor
 
 
 class Application:
- 
+    url = 'http://127.0.0.1:5000/maps'  # TODO: better organize urls
+
     def __init__(self, master):
         self.master = master
         self.master.geometry("1000x1000")
         self.master.title('Menu')
         self.master.bind_all('<Destroy>', self.on_destroy)
 
-        self.button_exit = Button(self.master, 
-                   text ="EXIT", 
+        self.button_exit = Button(self.master,
+                   text ="EXIT",
                    command = self.exit,
                    bg = 'thistle2',
                    activebackground = 'thistle1')
 
         self.button_exit.place(x = 350,
-                y = 550, 
-                height = 90, 
-                width = 300, 
+                y = 450,
+                height = 90,
+                width = 300,
                 bordermode='outside')
 
 
-        self.button = Button(self.master, 
-                   text ="MAP", 
+        self.button = Button(self.master,
+                   text ="MAP",
                    command = self.draw_map,
                    bg = 'thistle2',
                    activebackground = 'thistle1')
 
         self.button.place(x = 350,
-                y = 450, 
-                height = 90, 
-                width = 300, 
+                y = 350,
+                height = 90,
+                width = 300,
                 bordermode='outside')
 
 
-        self.button_make_map = Button(self.master, 
-                   text ="MAKE MAP", 
+        self.button_make_map = Button(self.master,
+                   text ="MAKE MAP",
                    command = self.make_map,
                    bg = 'thistle2',
                    activebackground = 'thistle1')
 
         self.button_make_map.place(x = 350,
-                y = 350, 
-                height = 90, 
-                width = 300, 
+                y = 250,
+                height = 90,
+                width = 300,
+                bordermode='outside')
+
+        self.button_upload_map = Button(self.master,
+                   text ="UPLOAD MAP",
+                   command = self.upload_map,
+                   bg = 'thistle2',
+                   activebackground = 'thistle1')
+
+        self.button_upload_map.place(x = 350,
+                y = 550,
+                height = 90,
+                width = 300,
+                bordermode='outside')
+
+        self.button_download_map = Button(self.master,
+                   text ="DOWNLOAD MAP",
+                   command = self.download_map,
+                   bg = 'thistle2',
+                   activebackground = 'thistle1')
+
+        self.button_download_map.place(x = 350,
+                y = 650,
+                height = 90,
+                width = 300,
                 bordermode='outside')
 
         self.master.mainloop()
@@ -81,28 +108,44 @@ class Application:
                 direction_path.append((next_cell[0] - current_cell[0], next_cell[1] - current_cell[1]))
             return direction_path
 
-        def f(array: [[int]]):
-            string_array = []
-            for subarray in array:
-                string_array.append(''.join(map(str,subarray)))
-            return string_array
-
         self.master.withdraw()
         file_name = askopenfilename()
 
         with open(file_name, 'r') as file:
             field_data = load(file)
         start = field_data['start']
-        goal = field_data['finish']
+        finish = field_data['finish']
         field = field_data['field']
 
-        url = 'http://127.0.0.1:5000'  # TODO: better organize urls
-        form = json.dumps({'start': start, 'goal': goal, 'field': f(field)})
-        path_length, path = loads(post(url, json=form).content)
+        form = dumps({'start': start, 'finish': finish, 'field': field, 'name': PurePath(file_name).name})
+        post(self.url, json=form)
 
-        # path_length, path = A_star.call(start, goal, f(field))
+        path_length, path = loads(get(self.url + '/test2.json').content)
+
         direction_path = get_direction_path(path) # путь из дельт
-        
-        
-        draw(self.master, Map(len(field), len(field[0]), 
-            field, start, goal, direction_path, -1))
+
+
+        draw(self.master, Map(len(field), len(field[0]),
+            field, start, finish, direction_path, -1))
+
+    def upload_map(self):
+        file_name = askopenfilename()
+        with open(file_name, 'r') as file:
+            field_data = load(file)
+        start = field_data['start']
+        finish = field_data['finish']
+        field = field_data['field']
+
+        form = dumps({'start': start, 'finish': finish, 'field': field, 'name': PurePath(file_name).name})
+        post(self.url, json=form)
+
+    def download_map(self):
+        all_maps = loads(get(self.url).content)
+        names = list(all_maps)
+        print(all_maps)
+        name = askstring('Choose map', 'map_name.json')
+        if name not in names:
+            return
+        file_name = asksaveasfilename()
+        with open(file_name, 'w') as file:
+            dump(all_maps[name], file)
--- service/app.py
+++ service/app.py
@@ -4,8 +4,30 @@ from src.a_star import A_star
 
 app = Flask(__name__)
 
+fields = {}
 
-@app.route("/", methods=['POST'])
-def get_solution():
-    form = loads(request.json)
-    return dumps(A_star.call(form['start'], form['goal'], form['field']))
+
+def f(array: [[int]]):
+    string_array = []
+    for subarray in array:
+        string_array.append(''.join(map(str, subarray)))
+    return string_array
+
+
+@app.route('/maps/', methods=['GET', 'POST'])
+def process_maps():
+    if request.method == 'POST':
+        field = loads(request.json)
+        field_name = field['name']
+        fields[field_name] = field
+        return str(len(fields) - 1)
+    if request.method == 'GET':
+        return dumps(fields)
+
+
+@app.route('/maps/<field_name>', methods=['GET'])
+def get_solution(field_name):
+    if field_name not in fields:
+        return 'Unknown map name'
+    field = fields[field_name]
+    return dumps(A_star.call(field['start'], field['finish'], f(field['field'])))
